866b6b9d21a633ff386d9985340b08478bc17414
oskarivilen <oskari.vilen@gmail.com>
2020-10-29 10:38:10 +0200

    first commit

---
+++ src/main/java/HH/SWD4TN022/QApp/QAppApplication.java
@@ -0,0 +1,13 @@
+package HH.SWD4TN022.QApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HH/SWD4TN022/QApp/QAppApplicationTests.java
@@ -0,0 +1,13 @@
+package HH.SWD4TN022.QApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
