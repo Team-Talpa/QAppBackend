2e11ed4e6d8f8b19c774aa9a6b240d7bee19987f
Melissa W <melissa.westberg@myy.haaga-helia.fi>
2020-11-03 16:28:08 +0200

    added Survey methods, SurveyRepository, SurveyController, surveylist.html

--- src/main/java/HH/SWD4TN022/QApp/domain/Survey.java
+++ src/main/java/HH/SWD4TN022/QApp/domain/Survey.java
@@ -1,9 +1,66 @@
 package HH.SWD4TN022.QApp.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Survey {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long surveyId;
+	private String surveyHeader;
+	private List<Question> questions;
 	
+	public Survey() {
+		super();
+		this.surveyHeader = null;
+		this.questions = new ArrayList<>();
+	}
+	
+	public Survey(String header) {
+		super();
+		this.surveyHeader = header;
+		this.questions = new ArrayList<>();
+	}
+	
+	public Survey(String header, List<Question> questions) {
+		super();
+		this.surveyHeader = header;
+		this.questions = questions;
+	}
+
+	public Long getSurveyId() {
+		return surveyId;
+	}
+
+	public void setSurveyId(Long surveyId) {
+		this.surveyId = surveyId;
+	}
+
+	public String getSurveyHeader() {
+		return surveyHeader;
+	}
+
+	public void setSurveyHeader(String surveyHeader) {
+		this.surveyHeader = surveyHeader;
+	}
+
+	public List<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}
+
+	@Override
+	public String toString() {
+		return "Survey [surveyId=" + surveyId + ", surveyHeader=" + surveyHeader + "]";
+	}
 	
 }
--- src/main/java/HH/SWD4TN022/QApp/web/SurveyController.java
+++ src/main/java/HH/SWD4TN022/QApp/web/SurveyController.java
@@ -1,5 +1,25 @@
 package HH.SWD4TN022.QApp.web;
 
-public class SurveyController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import HH.SWD4TN022.QApp.domain.SurveyRepository;
+
+@CrossOrigin
+@Controller
+public class SurveyController {
+	
+	@Autowired
+	private SurveyRepository surveyrepository;
+	
+	@GetMapping({"/", "/surveylist"})
+	public String surveyList(Model model) {
+		model.addAttribute("surveys", surveyrepository.findAll());
+		return "surveylist";
+	}
+	
+	
 }
