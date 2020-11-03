4fb517eab5465512d02301de288f49fe6d8e3e26
KingHenx <henkka.rossi@gmail.com>
2020-11-03 17:26:44 +0200

    Question class and controller

--- src/main/java/HH/SWD4TN022/QApp/web/QuestionController.java
+++ src/main/java/HH/SWD4TN022/QApp/web/QuestionController.java
@@ -1,5 +1,33 @@
 package HH.SWD4TN022.QApp.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import HH.SWD4TN022.QApp.domain.Question;
+
+@Controller
 public class QuestionController {
 
+	@RequestMapping("/questions")
+	public String questions(Model model) {
+		
+		//model.addAttribute("questions", questions);
+		
+		return "questions";
+	}
+	
+	@RequestMapping(value = "/add")
+	public String addquestion(Model model) {
+		model.addAttribute("question", new Question());
+		return "addquestion";
+	}
+	
+
+	
 }
+
+
