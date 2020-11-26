712f86bf9b2afa5f0b64bf4f460187ca6160babc
oskarivilen <oskari.vilen@gmail.com>
2020-11-26 11:28:06 +0200

    Demo data with answers

--- src/main/java/HH/SWD4TN022/QApp/QAppApplication.java
+++ src/main/java/HH/SWD4TN022/QApp/QAppApplication.java
@@ -10,7 +10,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
+
 import HH.SWD4TN022.QApp.domain.Answer;
+import HH.SWD4TN022.QApp.domain.AnswerOption;
+import HH.SWD4TN022.QApp.domain.AnswerOptionRepository;
 import HH.SWD4TN022.QApp.domain.AnswerRepository;
 import HH.SWD4TN022.QApp.domain.Question;
 import HH.SWD4TN022.QApp.domain.QuestionRepository;
@@ -26,10 +30,14 @@ public class QAppApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner demo(SurveyRepository surveyRepository, QuestionRepository questionRepository, AnswerRepository answerRepository) {
+	public CommandLineRunner demo(SurveyRepository surveyRepository, QuestionRepository questionRepository, AnswerRepository answerRepository, 
+			AnswerOptionRepository answeroptionrepository) {
+		
 		return (args) -> {
 			
 			//V13, HH-demosurvey
+			
+			/*
 			
 			Survey demoSurvey = new Survey("Hyvinvointikysely Haaga-Helian opiskelijoille");
 			
@@ -52,7 +60,54 @@ public class QAppApplication {
 			questionRepository.save(q6);
 			questionRepository.save(q7);
 			
+			*/
 			
+			// Demo vastausten kanssa
+			
+			
+			
+			Survey answerSurvey = new Survey ("Haaga-Helian kouluruokakysely");
+			
+			surveyRepository.save(answerSurvey);
+			
+			Question q1 = new Question("Ruokailetko millä kampuksella?", answerSurvey);
+			Question q2 = new Question("Kuinka monta kertaa viikossa käyt koulussa syömässä?", answerSurvey);
+			Question q3 = new Question("Monta kertaa viikosta otat kasvisvaihtoehdon?", answerSurvey);
+			Question q4 = new Question("Ostatko Premium vai Halpis-ruoan?", answerSurvey);
+			Question q5 = new Question("1-5 Kuinka tyytyväinen olet kouluruokaan?", answerSurvey);
+			
+			AnswerOption q5a1 = new AnswerOption("1" , q5);
+			AnswerOption q5a2 = new AnswerOption("2" , q5);
+			AnswerOption q5a3 = new AnswerOption("3" , q5);
+			AnswerOption q5a4 = new AnswerOption("4" , q5);
+			AnswerOption q5a5 = new AnswerOption("5" , q5);
+			
+			AnswerOption q4a1 = new AnswerOption("Premium" , q4);
+			AnswerOption q4a2 = new AnswerOption("Halpis" , q4);
+			
+			AnswerOption q3a1 = new AnswerOption("1-2" , q3);
+			AnswerOption q3a2 = new AnswerOption("3-4" , q3);
+			AnswerOption q3a3 = new AnswerOption("5" , q3);
+			
+			AnswerOption q2a1 = new AnswerOption("1-2" , q2);
+			AnswerOption q2a2 = new AnswerOption("3-4" , q2);
+			AnswerOption q2a3 = new AnswerOption("5" , q2);
+			
+			AnswerOption q1a1 = new AnswerOption("Porvoo" , q1);
+			AnswerOption q1a2 = new AnswerOption("Pasila" , q1);
+			AnswerOption q1a3 = new AnswerOption("Haaga" , q1);
+			
+			questionRepository.save(q1);
+			questionRepository.save(q2);
+			questionRepository.save(q3);
+			questionRepository.save(q4);
+			questionRepository.save(q5);
+			
+			answerRepository.save(new Answer(q1a2.getAnswerOptionBody(), q1));
+			answerRepository.save(new Answer(q2a3.getAnswerOptionBody(), q2));
+			answerRepository.save(new Answer(q3a2.getAnswerOptionBody(), q3));
+			answerRepository.save(new Answer(q4a2.getAnswerOptionBody(), q4));
+			answerRepository.save(new Answer(q5a1.getAnswerOptionBody(), q5));
 			
 			log.info("list all surveys");
 
